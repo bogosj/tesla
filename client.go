9480e6cb90a5da55c0fe744fa69c0463feef61a7
andig <cpuidle@gmx.de>
2021-03-26 08:27:13 +0100

    Clarify code

--- client.go
+++ client.go
@@ -71,7 +71,9 @@ func NewClient(ctx context.Context, options ...ClientOption) (*Client, error) {
 
 	client.ts = client.oc.TokenSource(ctx, client.token)
 	client.hc = oauth2.NewClient(ctx, client.ts)
-	// client.hc.Transport = &Transport{RoundTripper: client.hc.Transport}
+
+	// use the Tesla UA transport
+	client.hc.Transport = &Transport{RoundTripper: client.hc.Transport}
 
 	return client, nil
 }
