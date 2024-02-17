ee5d69104eeca3e6d275494f26b87794144b0094
andig <cpuidle@gmx.de>
2024-02-17 11:51:32 +0100

    Allow updating base url after client has been created

--- client.go
+++ client.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"strings"
 
 	"golang.org/x/oauth2"
 )
@@ -14,6 +15,11 @@ import (
 // Set to true to make the json decoder fail to decode if it encounters an
 // unknown field.
 const disallowUnknownFields = false
+
+const (
+	FleetAudienceNA = "https://fleet-api.prd.na.vn.cloud.tesla.com/"
+	FleetAudienceEU = "https://fleet-api.prd.eu.vn.cloud.tesla.com/"
+)
 
 // Client provides the client and associated elements for interacting with the Tesla API.
 type Client struct {
@@ -26,7 +32,7 @@ type Client struct {
 // functional options to initialize the client with an OAuth token.
 func NewClient(ctx context.Context, options ...ClientOption) (*Client, error) {
 	client := &Client{
-		baseURL: "https://owner-api.teslamotors.com/api/1",
+		baseURL: FleetAudienceNA + "api/1",
 	}
 
 	for _, option := range options {
@@ -40,7 +46,7 @@ func NewClient(ctx context.Context, options ...ClientOption) (*Client, error) {
 		client.hc = http.DefaultClient
 
 		if client.ts == nil {
-			return nil, errors.New("a token source must be provided")
+			return nil, errors.New("missing token source")
 		}
 
 		client.hc.Transport = &oauth2.Transport{
@@ -52,14 +58,25 @@ func NewClient(ctx context.Context, options ...ClientOption) (*Client, error) {
 	return client, nil
 }
 
+// Sets new base url. Use after obtaining user's region.
+func (c *Client) SetBaseUrl(url string) {
+	c.SetApiUrl(strings.TrimRight(url, "/") + "/api/1")
+}
+
+// Sets new api url. Use after obtaining user's region.
+// It is clients responsibility to append `/api/1` for use with the Fleet API.
+func (c *Client) SetApiUrl(url string) {
+	c.baseURL = url
+}
+
 // Calls an HTTP GET
-func (c Client) get(url string) ([]byte, error) {
+func (c *Client) get(url string) ([]byte, error) {
 	req, _ := http.NewRequest("GET", url, nil)
 	return c.processRequest(req)
 }
 
 // getJSON performs an HTTP GET and then unmarshals the result into the provided struct.
-func (c Client) getJSON(url string, out interface{}) error {
+func (c *Client) getJSON(url string, out interface{}) error {
 	body, err := c.get(url)
 	if err != nil {
 		return err
@@ -75,13 +92,13 @@ func (c Client) getJSON(url string, out interface{}) error {
 }
 
 // Calls an HTTP POST with a JSON body
-func (c Client) post(url string, body []byte) ([]byte, error) {
+func (c *Client) post(url string, body []byte) ([]byte, error) {
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(body))
 	return c.processRequest(req)
 }
 
 // Processes a HTTP POST/PUT request
-func (c Client) processRequest(req *http.Request) ([]byte, error) {
+func (c *Client) processRequest(req *http.Request) ([]byte, error) {
 	c.setHeaders(req)
 	res, err := c.hc.Do(req)
 	if err != nil {
@@ -95,7 +112,7 @@ func (c Client) processRequest(req *http.Request) ([]byte, error) {
 }
 
 // Sets the required headers for calls to the Tesla API
-func (c Client) setHeaders(req *http.Request) {
+func (c *Client) setHeaders(req *http.Request) {
 	req.Header.Set("Accept", "application/json")
 	req.Header.Set("Content-Type", "application/json")
 }
