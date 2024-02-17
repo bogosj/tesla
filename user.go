06019d1f3ca52a581180d40b0b68da1869a975b4
andig <cpuidle@gmx.de>
2024-02-17 12:26:45 +0100

    Fix region

--- user.go
+++ user.go
@@ -1,15 +1,18 @@
 package tesla
 
 type RegionResponse struct {
+	Response *Region `json:"response"`
+}
+type Region struct {
 	Region          string `json:"region"`
 	FleetApiBaseUrl string `json:"fleet_api_base_url"`
 }
 
 // UserRegion fetches the users region
-func (c *Client) UserRegion() (*RegionResponse, error) {
+func (c *Client) UserRegion() (*Region, error) {
 	var regionResponse RegionResponse
 	if err := c.getJSON(c.baseURL+"/users/region", &regionResponse); err != nil {
 		return nil, err
 	}
-	return &regionResponse, nil
+	return regionResponse.Response, nil
 }
